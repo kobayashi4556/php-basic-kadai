a147f8dbd7ec410f944284b0c1a0695cd115d147
kobayashi4556 <132803244+kobayashi4556@users.noreply.github.com>
2026-04-16 21:57:00 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,11 @@
+<?php
+// Step2. 連想配列を作成し、変数に代入する
+$onion_data = [
+    'name' => 'onion',
+    'price' => 200,
+    'weight' => 160,
+];
+
+// Step3. print_r関数を使いブラウザに連想配列を出力する
+print_r($onion_data);
+?>
